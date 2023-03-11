052e4b5901b2c5f844f6023cfbada83a3e0c6187
Chase Harrison <chaseh88@gmail.com>
2023-03-10 22:58:11 -0600

    add user model to user create route

--- src/pages/api/user/create.ts
+++ src/pages/api/user/create.ts
@@ -1,7 +1,18 @@
 import { hashPassword } from "../../../utils/password";
 import { NextApiRequest, NextApiResponse } from "next";
+import { User, UserInterface } from "../../../models/user";
+import { connect } from "../../../utils/server";
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+export type Response =
+  | {
+      message: string;
+      data: UserInterface;
+    }
+  | {
+      error?: string;
+    };
+
+const handler = async (req: NextApiRequest, res: NextApiResponse<Response>) => {
   try {
     if (req.method !== "POST") {
       throw new Error("Method not allowed");
@@ -13,15 +24,23 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       throw new Error("Please provide an email and password");
     }
 
+    await connect();
+
     const hashedPassword = await hashPassword(password);
 
-    console.log({
+    const user = new User({
       email,
       password: hashedPassword,
     });
 
-    res.status(200).json({ message: "User created" });
+    await user.save();
+
+    res.status(200).json({
+      message: "User created",
+      data: user,
+    });
   } catch (error: any) {
+    console.log(error);
     res.status(500).json({ error: error.message });
   }
 };
