5a8f9020c9e405a74a7b4fd5ecc5e5a06747a0da
Chase Harrison <chaseh88@gmail.com>
2023-03-11 20:57:40 -0600

    update user faker

--- src/seeder/seeder.ts
+++ src/seeder/seeder.ts
@@ -1,12 +1,24 @@
-import { userFaker } from "./user-faker";
+import { userFaker, defaultUsersFaker } from "./user-faker";
 import { User } from "models/user";
 import { connect, close } from "utils/server";
+import { UserInterface } from "types/interfaces";
 
 const seeder = async () => {
+  // Connect to the database
   await connect();
   console.log("Seeding database...");
+
+  // Seed the database
+  const users: UserInterface[] = [...(await defaultUsersFaker())];
+
+  Array.from({ length: 10 }).forEach(async () => {
+    users.push(await userFaker());
+  });
+
   await User.deleteMany({});
-  await User.insertMany(await userFaker(10));
+  await User.insertMany(users);
+
+  // Close the connection
   await close();
   console.log("Database seeded");
 };
--- src/seeder/user-faker.ts
+++ src/seeder/user-faker.ts
@@ -3,30 +3,30 @@ import { UserInterface } from "types/interfaces";
 import { UserRoles } from "types/types";
 import { hashPassword } from "utils/password";
 
-export const userFaker = async (number: number): Promise<UserInterface[]> => {
-  const users: UserInterface[] = [
-    {
-      id: faker.database.mongodbObjectId(),
-      email: "ChaseH88@gmail.com",
-      password: await hashPassword("Password12345!!"),
-      role: "admin",
-    },
-  ];
+/**
+ * Faker to create default users for the system
+ * @returns
+ */
+export const defaultUsersFaker = async (): Promise<UserInterface[]> => [
+  {
+    id: faker.database.mongodbObjectId(),
+    email: "ChaseH88@gmail.com",
+    password: await hashPassword("Password12345!!"),
+    role: "admin",
+  },
+];
 
-  for (let i = 1; i < number; i++) {
-    const user: UserInterface = {
-      id: faker.database.mongodbObjectId(),
-      email: faker.internet.email(),
-      password: await hashPassword(faker.internet.password()),
-      role: faker.helpers.arrayElement([
-        "customer",
-        "manager",
-        "admin",
-      ]) as UserRoles,
-    };
-
-    users.push(user);
-  }
-
-  return users;
-};
+/**
+ * Faker to create a new user
+ * @returns
+ */
+export const userFaker = async (): Promise<UserInterface> => ({
+  id: faker.database.mongodbObjectId(),
+  email: faker.internet.email(),
+  password: await hashPassword(faker.internet.password()),
+  role: faker.helpers.arrayElement([
+    "customer",
+    "manager",
+    "admin",
+  ]) as UserRoles,
+});
