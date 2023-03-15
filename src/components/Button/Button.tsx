1459fa47fb1156db8b87f35274cbd27d6388bc17
Chase Harrison <chaseh88@gmail.com>
2023-03-14 21:48:50 -0500

    working on button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,18 +1,66 @@
-import { MotionStyle } from "framer-motion";
+import React, { useState } from "react";
+import { motion, AnimatePresence, useAnimation } from "framer-motion";
 import { ButtonStyled, ButtonStyledProps } from "./styles";
 
 interface ButtonProps extends ButtonStyledProps {
   children?: React.ReactNode;
   className?: string;
   id?: string;
-  style?: MotionStyle;
   onClick?: () => void;
 }
 
 export const Button = (props: ButtonProps) => {
+  const [ripple, setRipple] = useState({ active: false, x: 0, y: 0 });
+  const rippleControls = useAnimation();
+
+  const handleMouseDown = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const rect = e.currentTarget.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    setRipple({ active: true, x, y });
+  };
+
+  const handleMouseUp = () => {
+    setRipple((prev) => ({ ...prev, active: false }));
+  };
+
+  const handleMouseLeave = () => {
+    setRipple((prev) => ({ ...prev, active: false }));
+  };
+
   return (
-    <ButtonStyled style={props.style} {...props}>
-      {props.children}
+    <ButtonStyled
+      {...props}
+      onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}
+      onMouseLeave={handleMouseLeave}
+      size={props.size || "medium"}
+    >
+      <span>{props.children}</span>
+      <AnimatePresence>
+        {ripple.active && (
+          <motion.span
+            initial={{ opacity: 1, scale: 0 }}
+            animate={{ opacity: 0.1, scale: 50 }}
+            exit={{ opacity: 0, scale: 0 }}
+            transition={{
+              duration: 0.5,
+              ease: "easeInOut",
+            }}
+            style={{
+              position: "absolute",
+              background: "#ffffff61",
+              borderRadius: "50%",
+              width: "20px",
+              height: "20px",
+              top: ripple.y - 10,
+              left: ripple.x - 10,
+              pointerEvents: "none",
+              opacity: 1,
+            }}
+          ></motion.span>
+        )}
+      </AnimatePresence>
     </ButtonStyled>
   );
 };
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -5,19 +5,60 @@ export interface ButtonStyledProps {
   variant: "primary" | "secondary";
   disabled?: boolean;
   activity?: boolean;
+  size: "small" | "medium" | "large";
 }
 
 export const primaryButton = css`
-  background: ${({ theme }) => theme.colors.primary};
+  background: transparent;
   color: ${({ theme }) => theme.colors.text};
+  border-color: ${({ theme }) => theme.colors.text};
+  &:hover,
+  &:focus {
+    background: ${({ theme }) => theme.colors.text};
+    color: ${({ theme }) => theme.colors.background};
+  }
 `;
 
 export const secondaryButton = css`
   background: ${({ theme }) => theme.colors.secondary};
   color: ${({ theme }) => theme.colors.textSecondary};
+  border-color: ${({ theme }) => theme.colors.textSecondary};
+  &:hover,
+  &:focus {
+    background: ${({ theme }) => theme.colors.text};
+    color: ${({ theme }) => theme.colors.background};
+  }
+`;
+
+export const smallButton = css`
+  padding: ${({ theme }) => theme.spacing[1]} ${({ theme }) => theme.spacing[4]};
+  font-size: 0.8rem;
+  &:hover,
+  &:focus {
+    background: ${({ theme }) => theme.colors.text};
+    color: ${({ theme }) => theme.colors.background};
+  }
+`;
+
+export const mediumButton = css`
+  padding: ${({ theme }) => theme.spacing["2"]}
+    ${({ theme }) => theme.spacing[8]};
+  font-size: 1rem;
+`;
+
+export const largeButton = css`
+  padding: ${({ theme }) => theme.spacing[3]}
+    ${({ theme }) => theme.spacing[15]};
+  font-size: 1.2rem;
 `;
 
 export const ButtonStyled = styled(motion.button)<ButtonStyledProps>`
+  position: relative;
+  overflow: hidden;
+  user-select: none;
+  border: 1px solid;
+  cursor: pointer;
+  transition: all 0.3s ease;
   ${({ variant }) => (variant === "primary" ? primaryButton : secondaryButton)}
   ${({ disabled }) =>
     disabled &&
@@ -25,4 +66,10 @@ export const ButtonStyled = styled(motion.button)<ButtonStyledProps>`
       opacity: 0.5;
       cursor: not-allowed;
     `}
+    ${({ size }) =>
+    size === "small"
+      ? smallButton
+      : size === "medium"
+      ? mediumButton
+      : largeButton};
 `;
