1d6223a8c8ee902d30336acceb4f781d6034f027
Chase Harrison <chaseh88@gmail.com>
2023-03-22 07:32:33 -0500

    enhance the me query to return the entire cart

--- src/pages/api/user/me.ts
+++ src/pages/api/user/me.ts
@@ -1,6 +1,12 @@
 import { NextApiResponse } from "next";
 import { User } from "models/user";
-import { CustomRequest, UserInterface } from "types/interfaces";
+import { Product } from "models/product";
+import {
+  CartItemInterface,
+  CustomRequest,
+  ProductInterface,
+  UserInterface,
+} from "types/interfaces";
 import { Response } from "types/types";
 import { connectHandler } from "utils";
 
@@ -10,10 +16,39 @@ const handler = connectHandler(
     isProtected: true,
   },
   async (req: CustomRequest, res: NextApiResponse<Response<UserInterface>>) => {
-    const user = await User.findById(req.userId).select("-password");
+    const user = (await User.findById(req.userId)
+      .select("-password")
+      .populate({
+        path: "cart",
+        populate: [
+          {
+            path: "productId",
+            model: Product,
+          },
+        ],
+      })) as UserInterface;
+
+    const cart = user?.cart?.map(({ productId, variantId, quantity }) => {
+      const variantDoc = (
+        productId as unknown as ProductInterface
+      ).variants.find(
+        (variant) => variant._id?.toString() === variantId.toString()
+      );
+      return {
+        productId,
+        variantId: variantDoc,
+        quantity,
+      };
+    }) as unknown as CartItemInterface[];
+
+    const formattedUser: UserInterface = {
+      ...(user as any)._doc,
+      cart,
+    };
+
     return res
       .status(200)
-      .json({ message: "User has been found.", data: user });
+      .json({ message: "User has been found.", data: formattedUser });
   }
 );
 
